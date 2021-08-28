18d59dc9fa81287e177552e8800f0d9535a5da73
pzych <pzych@visionapartments.com>
2021-08-28 08:22:54 +0200

    added esion with custom interface sessiond ata

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import session from 'express-session';
 import UserService from "./user-service";
 
 const app = express();
+app.use(session({secret: 'keyboard cat', cookie: {maxAge: 60000}}))
 
-let a = 1;
 app.get('/', async (req, res) => {
+    req.session.views = req.session.views ? req.session.views + 1 : 1;
+
     let userService = new UserService();
     let user = await userService.getUser();
-    res.send('Well done!'  + a++ + JSON.stringify(user));
+
+    res.send('Well done!' + req.session.views + JSON.stringify(user));
 })
 
 app.listen(3000, () => {
