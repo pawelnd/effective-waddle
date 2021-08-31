19fdef460ed63234f0e89de8140b15ad3dd748ba
pzych <pawel.zych@gmail.com>
2021-08-31 07:48:42 +0200

    dasdsad

--- src/user-service.test.ts
+++ src/user-service.test.ts
@@ -1,14 +1,9 @@
 import UserService from "./user-service";
 import {getUser} from "./get-user";
+import {UserDto} from "./UserDto";
 
-jest.mock('./get-user', () => ({
-    getUser: jest.fn(() => Promise.resolve({
-        fullName: 'Terefere',
-        email: 'Terefere',
-        password: 'Terefere',
-    }))
+jest.mock('./get-user');
 
-}))
 describe('The Test service', () => {
     // xit('should get data from repo', async () => {
     //
@@ -22,6 +17,13 @@ describe('The Test service', () => {
     // });
 
     it('should get data from repo', async () => {
+        const mockedGetUser = getUser as unknown as jest.MockedFunction<typeof getUser>
+
+        mockedGetUser.mockResolvedValue({
+            fullName: "Terefere",
+            email: 'Terefere',
+            password: 'Terefere',
+        })
         const userService = new UserService();
         // await userService.getUser();
 
